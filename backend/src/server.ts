e6de1af113a454b7f169398a4cc2c01a92b27e37
Satyam Sakpal <sakpalsatyam3@gmail.com>
2024-11-15 17:14:57 +0530

    minor update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,10 +13,10 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3000 
 
-// app.use(express.static(path.join(__dirname, "../../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../build")));
 
 // app.get('/',(req, res) => {
-//     res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+//     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 // })
 
 app.post('/word/check/', (req, res) => {
